fe21b46b9f009d334869ef01bed576c1c340dcd8
Muhammad Zain Ul Abideen <241475@students.au.edu.pk>
2025-07-08 19:17:17 +0500

    Forward Adding

--- today.cpp
+++ today.cpp
@@ -6,30 +6,31 @@ class Node{
   int data;
   Node * next;
 
-  Node(int value){
-    data = value;
-    next = NULL;
+  Node (int value){
+     data = value;
+     next = NULL;
   }
-
 };
 
 int main(){
 
-int array [] = {2,3,4,5};
+  int array [] = {2,3,4,5};
+ Node * head;
+ head = NULL;
 
-Node * head;
-
-for(int i = 0;i <4 ;i++){
-    if(head == NULL){
-        head = new Node(array[i]);
-    }
-    else{
-        Node * temp;
-        temp = new Node(array[i]);
-        temp->next = head;
-        head = temp;
-    }
+  for(int i = 0;i<4;i++){
+ if(head==NULL){
+  head = new Node(array[i]);
+ }
+else{
+  Node * temp = new Node(array[i]);
+  temp->next = head;
+  head = temp;
+}
 }
 
-
+while(head){
+cout<<head->data<<endl;
+head= head->next;
+}
 }
