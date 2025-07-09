6b36d2256a3d408e0b31dab0b8542963c6050bb2
Muhammad Zain Ul Abideen <241475@students.au.edu.pk>
2025-07-09 17:01:06 +0500

    Forward and Backward Recursion on Single LinkedList

--- today.cpp
+++ today.cpp
@@ -12,6 +12,7 @@ class Node{
   }
 };
 
+//Backward adding
 Node * BackwardLinkedlist(int array [],int size,int index){
   //Base Case
   if(index==size){
@@ -22,15 +23,32 @@ Node * BackwardLinkedlist(int array [],int size,int index){
   return temp;
 }
 
+//Forward Adding
+
+Node * ForwardLinkedList(int array[],int size,int index,Node* prev){
+  //Base Case
+  if(size==index){
+    return prev;
+  }
+  Node * temp = new Node(array[index]);
+  temp->next=prev;
+  return ForwardLinkedList(array,size,index+1,temp);
+}
+
+
 int main(){
   int array [] = {2,4,6,8};
   
   Node * head ;
   head = NULL;
 
-  head = BackwardLinkedlist(array,4,0);
- 
-  //printing
+//For Backward
+ // head = BackwardLinkedlist(array,4,0);
+   
+//For Forward
+  head = ForwardLinkedList(array,4,0,NULL);
+  
+//printing
   while(head!=NULL){
     cout<<head->data<<endl;
     head = head->next;
