689a381cbc8eeb1ada19ce5d343f158e08028cf0
Muhammad Zain Ul Abideen <241475@students.au.edu.pk>
2025-07-12 17:08:14 +0500

    Insertion at Nth Position in Double Linkedlist - C++

--- today.cpp
+++ today.cpp
@@ -15,16 +15,45 @@ class Node{
   }
 };
 
+
+//Function to insert a Node at nth Position in Double LinkedList
+Node * insertionAtNthPosition(Node * head,int position,int value){
+  if(head==NULL){
+    return head;
+  }
+  if(position <=0){
+    return head;
+  }
+
+  Node * current = head;
+  Node * prev = NULL;
+
+  for(int i = 0  ; i < position-1;i++){
+     prev = head;
+     current = current->next;
+  }
+
+  Node * temp = new Node (value);
+  prev->next  = temp;
+  temp->prev=prev;
+  temp->next = current;
+  current->prev=temp;
+
+  return head;
+  
+}
+
+
 int main(){
 
-int array [] = {2,4,6,8,10};
+int array [] = {1,3,4,5};
 
 //Making pointer to track and tranverse Linkedlist
 Node * head = NULL;
 Node * lastNode = NULL;
 
 //Loop to Make and assign values to the Linkedlist
-for(int i = 0 ; i < 5;i++){
+for(int i = 0 ; i < 4;i++){
   //Creating Head
   if(head==NULL){
     head = new Node(array[i]);
@@ -42,12 +71,22 @@ for(int i = 0 ; i < 5;i++){
 //Making a new pointer to tranverse the Linkedlist and print it out
 Node * current = head;
 
-//Printing The Doubled LinkedList
+//Printing The Doubled LinkedList before insertion at nth Position
 while(current){
-  cout<<current->data<<" Previous : "<<current->prev<<" Next : "<<current->next<<endl;
+  cout<<current->data<<endl;
   current= current->next;
 }
 
+//Creating new Pointer to catch Address of head after function call
+Node * temp = NULL;
+temp = insertionAtNthPosition(head,2,2);
+current = temp;
+
+//Printing The Doubled LinkedList After the insertion at nth Position
+while(current){
+  cout<<current->data<<endl;
+  current= current->next;
+}
 
 }
 
