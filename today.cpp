4f897fdd79d9594fb064dc7336e220947f3aef0e
Muhammad Zain Ul Abideen <241475@students.au.edu.pk>
2025-07-12 16:54:57 +0500

    DoubleLinkedList Syntax and Data insertion - C++

--- today.cpp
+++ today.cpp
@@ -1,89 +1,54 @@
 #include <iostream>
 using namespace std;
 
+//Double Linkedlist
 class Node{
   public:
   int data;
+  Node * prev;
   Node * next;
 
   Node (int value){
      data = value;
      next = NULL;
+     prev = NULL;
   }
 };
 
-
-//Backward adding using Recursion [1,2,3,4,...]
-Node * BackwardLinkedlist(int array [],int size,int index){
-  //Base Case
-  if(index==size){
-    return NULL;
-  }
-  Node * temp = new Node (array[index]);
-  temp->next=BackwardLinkedlist(array,size,index+1);
-  return temp;
-}
-
-//Rotating The LinkedList by nth Position 
-Node * RotateLinkedListbyNthPosition(Node * head,int position){
-   if (head == NULL) return NULL;
-   if (position == 0) return head;
-   int count = 0;
-   Node * temp = NULL;
-    temp = head;
-
-    while(temp){
-      temp = temp->next;
-      count++;
-    }
-
-    position = position % count;
-    Node * prev = NULL;
-    Node * current = head;
-    for (int i = 1 ; i <=count-position;i++){
-        prev = current;
-        current = current->next;
-    }
-     prev->next = NULL;
-     Node * last = current;
-
-     while(last->next!=NULL){
-      last = last->next;
-     }
-     last->next = head;
-     head = current;
-     return head;
-}
-
-
 int main(){
-  int array [] = {2,4,6,8,9,10};
-  
-  Node * head ;
-  head = NULL;
 
-//For Backward assigning of array to LinkedList [1,2,3,4...]
-  head = BackwardLinkedlist(array,6,0);
-   
-//Printing Before the Finding of the Middle of the LinkedList
-   Node * temp = head;
-  while(temp){
-    cout<<" Value :"<<temp->data<<" Address :"<<temp<<endl;
-    temp = temp->next;
+int array [] = {2,4,6,8,10};
+
+//Making pointer to track and tranverse Linkedlist
+Node * head = NULL;
+Node * lastNode = NULL;
+
+//Loop to Make and assign values to the Linkedlist
+for(int i = 0 ; i < 5;i++){
+  //Creating Head
+  if(head==NULL){
+    head = new Node(array[i]);
+    lastNode = head;
   }
-    cout<<endl;
-
-//nth Postion by which LinkedList is to Rotate e.g ; 3
-    int position = 3;
-
-//Function Calling
-   temp = RotateLinkedListbyNthPosition(head,position);
-
-//Printing After the Linkedlist is Rotated by nth Position
-   while(temp){
-    cout<<" Value :"<<temp->data<<" Address :"<<temp<<endl;
-    temp = temp->next;
+  //Creating Nodes next to the head such as tail
+  else{
+    Node * temp = new Node (array[i]);
+    lastNode->next=temp;
+    temp->prev=lastNode;
+    lastNode = temp;
   }
+}
+
+//Making a new pointer to tranverse the Linkedlist and print it out
+Node * current = head;
+
+//Printing The Doubled LinkedList
+while(current){
+  cout<<current->data<<" Previous : "<<current->prev<<" Next : "<<current->next<<endl;
+  current= current->next;
+}
 
 
 }
+
+
