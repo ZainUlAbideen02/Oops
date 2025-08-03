13fd4054045b8190e5a416ef336f647c54de6674
Muhammad Zain Ul Abideen <241475@students.au.edu.pk>
2025-08-03 13:51:23 +0500

    LeetCode Problem  - Loop Detection - Circular LinkedList - DSA C++

--- today.cpp
+++ today.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include<vector>
+#include <unordered_map>
 using namespace std;
 
 //Single LinkedList
@@ -14,20 +14,15 @@ class Node{
   }
 };
 
-bool check(vector<Node *>visited,Node * curr){
-   for(int i = 0 ; i<visited.size();i++ ){
-      if(visited[i]==curr){
-         return 1;
-      }
-   }
-   return 0 ;
-}
+
 
 int main(){
    int array [] = {1,2,3,4,5};
+
    Node * head = NULL;
    Node * current = NULL;
-
+  
+   //Assigning Values 
    for(int i = 0 ; i <5;i++){
       Node * temp = new Node(array[i]);
       if(head==NULL){
@@ -39,37 +34,23 @@ int main(){
          current = current->next;
       }
    }
-
+   //Loop Formation
    current->next= head;
-
+ 
+   //Making Pointer to Tranverse
    Node * curr = head;
-   vector <Node *> visited;
 
+   //Using UnOrdered_Map to reduce space and time complexity
+   unordered_map<Node * ,bool>visited;
+
+   //Checks For Loop
    while(curr!=NULL){
-      if(check(visited,curr)){
-        cout << "Cycle detected!" << endl;
-         return 0;
+      if(visited[curr]==1){
+         cout<<"Loop Detected"<<endl;
+         return 0 ;
       }
-      visited.push_back(curr);
+      visited[curr]=1;
       curr = curr->next;
    }
    
-
-
-   // Node* temp = head;
-   // if(temp != NULL){
-   //    do {
-   //       cout << temp->data << endl;
-   //       temp = temp->next;
-   //    } while(temp != head);
-   // }
-
-
-
 }
-
-
-
-
-  
-
