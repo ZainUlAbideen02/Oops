ac9e3e49fe3ebd8b2d9384c1b446601c3efd0afd
Muhammad Zain Ul Abideen <241475@students.au.edu.pk>
2025-08-03 15:33:02 +0500

    LeetCode - Breaking The Loop - Detection of Loop - Length of Loop - DSA C++ Problem

--- today.cpp
+++ today.cpp
@@ -14,16 +14,30 @@ class Node{
   }
 };
 
+//To Print the LinkedList
+void print(Node * head){
+    Node * temp3 = head;
+   while(temp3!=NULL){
+      cout<<temp3->data<<" ";
+      temp3 = temp3->next;
+   }
+}
 
 
 int main(){
-   int array [] = {1,2,3,4,5};
+   //Array , Whose Values is in LinkedList
+   int array [] = {1,2,3,4,5,6,7,8,9,10};
 
+   //Making Pointer to Point & Tranverse the Linkedlist
    Node * head = NULL;
    Node * current = NULL;
   
+   //Counting the Total Length of the array
+   int totallength = sizeof(array) / sizeof(array[0]);
+
    //Assigning Values 
-   for(int i = 0 ; i <5;i++){
+   for(int i = 0 ; i <totallength;i++){
+
       Node * temp = new Node(array[i]);
       if(head==NULL){
          head = temp;
@@ -34,23 +48,68 @@ int main(){
          current = current->next;
       }
    }
-   //Loop Formation
-   current->next= head;
- 
-   //Making Pointer to Tranverse
-   Node * curr = head;
-
-   //Using UnOrdered_Map to reduce space and time complexity
-   unordered_map<Node * ,bool>visited;
-
-   //Checks For Loop
-   while(curr!=NULL){
-      if(visited[curr]==1){
-         cout<<"Loop Detected"<<endl;
-         return 0 ;
-      }
-      visited[curr]=1;
-      curr = curr->next;
+   //Pointer to point to the Node Where Loop is to create 
+   //Suppose We Create Loop at 3rd Position
+   Node * temp = head;
+   for(int i = 0 ; i < 3;i++){
+      temp=temp->next;
    }
-   
-}
+   current->next=temp;
+
+    //Going to Use Floyd’s Cycle Detection Algorithm to detect the loop.
+      Node * slow = head;
+      Node * fast = head;
+
+   //To Count the Length Of the Loop
+      int looplen = 1;
+
+    while(slow!=NULL){
+      
+      //Moving slow pointer one step and fast two steps ahead
+      slow = slow->next;
+      fast = fast->next->next;
+      
+      //If Loops Found,They become equal to each other
+     if(slow==fast){
+      cout<<"Loop Present"<<endl;
+      fast = fast->next;
+      //Calculating the length of the Loop
+       while (fast!=slow)
+       {
+           looplen++;
+         fast=fast->next;
+       }
+       cout<<"The Length Of the Loop is "<<looplen<<endl;
+       
+       //Founding the index of Loop starting Node
+       int newlength = totallength-looplen;
+       cout<<"The Loop Present at Node No."<<newlength<<endl;
+
+       //Pointing the Pointer at the Loop Starter Node
+       Node * temp = head;
+       for(int i = 0 ;i<newlength;i++){
+         temp = temp->next;
+       }
+        
+       //Breaking the Loop 
+       while(fast->next!=temp || fast!=temp){
+         if(fast->next==temp){
+            fast->next=NULL;
+            cout<<"Loop Breaks"<<endl;
+            //Print the LinkedList After breaking the Loop
+            print(head);
+            return 0;
+         }
+         fast=fast->next;
+       }
+
+     }
+     //If Loop is not present
+     if(fast==NULL || fast->next==NULL ){
+      cout<<"No Loop Present"<<endl;
+      return 0;
+     }
+
+   }
+
+   }
