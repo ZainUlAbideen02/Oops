59ea18e73aed56bb79e059a28a5966b3b2a371aa
Muhammad Zain Ul Abideen <241475@students.au.edu.pk>
2025-08-04 16:55:19 +0500

    Reversing The Linkedlist From nth Position to mth position - LeetCode - DSA C++

--- today.cpp
+++ today.cpp
@@ -2,22 +2,22 @@
 using namespace std;
 
 // Single LinkedList Node definition
-class Node{
-  public:
-  int data;       // Data stored in the node
-  Node * next;    // Pointer to the next node
+class Node {
+public:
+    int data;       // Data stored in the node
+    Node* next;     // Pointer to the next node
 
-  // Constructor to initialize node with a value
-  Node (int value){
-     data = value;
-     next = NULL;
-  }
+    // Constructor to initialize node with a value
+    Node(int value) {
+        data = value;
+        next = NULL;
+    }
 };
 
 // Function to print the linked list
-void print(Node * head){
-    Node * temp3 = head;
-    while(temp3 != NULL){
+void print(Node* head) {
+    Node* temp3 = head;
+    while (temp3 != NULL) {
         cout << temp3->data << " ";  // Print current node's data
         temp3 = temp3->next;         // Move to the next node
     }
@@ -40,89 +40,75 @@ Node* reverse(Node* head) {
     return prev;  // New head of the reversed list
 }
 
-int main(){
-   // Arrays representing two numbers (digits stored in reverse order)
-   int array1 [] = {9,7,6,8,4};   // Represents number 48679
-   int array2 [] = {6,4,3,8};     // Represents number 8346
+// Function to get the last node of a linked list
+Node* lastnode(Node* head) {
+    Node* temp = head;
+    while (temp->next != NULL) {
+        temp = temp->next;  // Traverse to last node
+    }
+    return temp;
+}
 
-   // Initialize pointers for first linked list
-   Node * head1 = NULL;
-   Node * current = NULL;
-  
-   // Length of array1
-   int totallength = sizeof(array1) / sizeof(array1[0]);
+// Function to reverse a sublist between start and end indices
+Node* ngroups(Node* head, int start, int end) {
+    Node* first = NULL;
+    Node* sec = head;
 
-   // Convert array1 to linked list
-   for(int i = 0 ; i < totallength; i++){
-      Node * temp = new Node(array1[i]);  // Create new node
-      if(head1 == NULL){
-         head1 = temp;        // First node becomes head
-         current = temp;
-      }
-      else{
-         current->next = temp;  // Append to list
-         current = current->next;
-      }
-   }
+    // Traverse to the 'start' position
+    while (start--) {
+        first = sec;       // one node before the reversal segment
+        sec = sec->next;   // node at 'start' position
+    }
 
-   // Reuse current pointer for second list
-   current = NULL;
-   Node * head2 = NULL;
+    Node* third = NULL;
+    Node* fourth = head;
 
-   // Length of array2
-   totallength = sizeof(array2) / sizeof(array2[0]);
+    // Traverse to the 'end' position
+    while (end--) {
+        third = fourth;    // node at 'end' position
+        fourth = fourth->next;  // node just after the reversal segment
+    }
 
-   // Convert array2 to linked list
-   for(int i = 0 ; i < totallength; i++){
-      Node * temp = new Node(array2[i]);  // Create new node
-      if(head2 == NULL){
-         head2 = temp;        // First node becomes head
-         current = temp;
-      }
-      else{
-         current->next = temp;  // Append to list
-         current = current->next;
-      }
-   }
-   
-   // Reverse both linked lists to make addition easier (least significant digit first)
-   Node * temp = head1;
-   head1 = reverse(temp);
-   temp = head2;
-   head2 = reverse(temp);
+    // Disconnect sublist to reverse
+    first->next = NULL;
+    third->next = NULL;
 
-   // Prepare for addition
-   temp = head1;
-   Node * temp2 = head2;
-   Node * head3 = NULL;   // Head of the result list
-   Node * curr = NULL;    // Pointer to track current node in result
-   int carry = 0;         // Carry value for digit-wise addition
+    // Reverse the sublist between 'start' and 'end'
+    sec = reverse(sec);        // 'sec' is now the new head of reversed sublist
+    third = lastnode(sec);     // Get the new last node of reversed sublist
 
-   // Add digits from both lists with carry
-   while(temp != NULL || temp2 != NULL || carry > 0){
-       int val1 = (temp != NULL) ? temp->data : 0;  // Value from first list or 0
-       int val2 = (temp2 != NULL) ? temp2->data : 0;  // Value from second list or 0
-       int sum = val1 + val2 + carry;  // Sum of values and carry
-       carry = sum / 10;               // Update carry
+    // Reconnect the reversed sublist back to the list
+    first->next = sec;         // Connect first part to reversed sublist
+    third->next = fourth;      // Connect reversed sublist to remaining list
 
-       Node * newNode = new Node(sum % 10);  // Create node for current digit
-       if(head3 == NULL){
-           head3 = newNode;  // First digit
-           curr = newNode;
-       } else {
-           curr->next = newNode;  // Append to result
-           curr = curr->next;
-       }
+    return head;  // Return updated head (unchanged in this hardcoded example)
+}
 
-       // Move to next nodes if available
-       if (temp != NULL) temp = temp->next;
-       if (temp2 != NULL) temp2 = temp2->next;
-   }
+int main() {
+    int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};  // Array to convert to linked list
 
-   // Reverse the result list to get final number
-   Node * pr = head3;
-   pr = reverse(pr);
+    Node* head = NULL;
+    Node* current = NULL;
 
-   // Print the result
-   print(pr);
+    int totallength = sizeof(array) / sizeof(array[0]);
+
+    // Convert array to linked list
+    for (int i = 0; i < totallength; i++) {
+        Node* temp = new Node(array[i]);  // Create new node
+        if (head == NULL) {
+            head = temp;        // First node becomes head
+            current = temp;
+        } else {
+            current->next = temp;  // Append to list
+            current = current->next;
+        }
+    }
+
+    Node* temp = head;
+
+    // Reverse nodes from position 4 to 8 (hardcoded)
+    temp = ngroups(temp, 4, 8);
+
+    // Print updated list
+    print(temp);
 }
