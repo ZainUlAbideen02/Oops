38d4ec4022a853a5d26f63c84e3212ff6c553a8e
Muhammad Zain Ul Abideen <241475@students.au.edu.pk>
2025-08-08 12:48:27 +0500

    Stack - Next Greater Element to the right

--- today.cpp
+++ today.cpp
@@ -1,39 +1,31 @@
 #include <iostream>
-#include <vector>
 #include <stack>
 using namespace std;
 
 int main() {
-  vector<string>vec;
-  vec.push_back("ab");
-  vec.push_back("ac");
-  vec.push_back("da");
-  vec.push_back("da");
-  vec.push_back("ac");
-  vec.push_back("db");
-  vec.push_back("ea");
+    // Input array
+    int array [] = {8,6,4,7,4,9,10,8,12};
+    stack<int> store; // stack to store indices
+    int size = sizeof(array) / sizeof(array[0]);
 
+    int array1[size]; 
+    fill(array1, array1 + size, -1); // initialize result array with -1
 
-    vec.push_back("ea");
+    store.push(0); // push index 0 as starting point
 
-  stack<string>st;
-  st.push(vec[0]);
-  for(int i = 1 ; i<vec.size();i++){
-    if(vec[i]!=st.top()){
-        st.push(vec[i]);
+    // Loop through array (till second last element)
+    for(int i = 0; i < size - 1; i++) {
+        // While stack not empty and current next element is greater
+        while (!store.empty() && array[store.top()] < array[i+1]) {
+            array1[store.top()] = array[i+1]; // assign next greater
+            store.pop(); // remove index since it's resolved
+        }
+        // push the index of the next element
+        store.push(i+1);
     }
-    else{
-        st.pop();
+
+    // Print the results
+    for(int i : array1) {
+        cout << i << " ";
     }
-  }
-
-  vector<string> nw;
-   while(!st.empty()){
-    nw.push_back(st.top());
-    st.pop();
-   }
-
-   for(int i = 0 ; i< nw.size();i++){
-    cout<<nw[i]<<" ";
-   }
 }
